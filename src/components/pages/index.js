65fc602732ea65bf593df758680df89480a53de3
Jonathan <jrugama96@gmail.com>
2021-07-01 23:57:04 -0600

    feat: Se agrega la pagina index, y la respectiva tabla de Employees, sin conexion a la API (harcoded)

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -3,9 +3,30 @@ import React from 'react';
 const IndexPage = () => {
 
     return (
-        <div>
-            Holis
-        </div>
+    <div className="list-container">
+        <h2 className="list-title">Employees</h2>
+        <table className="table-list">
+           
+            <tbody className="table-body">
+                <tr className="table-row">
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Actions</th>
+                </tr>
+                <tr className="table-row">
+                    <td>Thomas Hardy</td>
+                    <td>thomashardy@mail.com</td>
+                    <td>edit - delete</td>
+                </tr>
+                <tr className="table-row">
+                    <td>Thomas Hardy</td>
+                    <td>thomashardy@mail.com</td>
+                    <td>edit - delete</td>
+                </tr>
+               
+            </tbody>
+        </table>
+    </div>
     )
 }
 
